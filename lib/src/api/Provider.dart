17a67107ab217c44827c2a0c7223ef1a457ee368
joshu <joshuapetitma@yahoo.com>
2020-11-26 09:08:08 -0500

    adds fields that are updated for ever new api implementation

--- lib/src/api/Provider.dart
+++ lib/src/api/Provider.dart
@@ -3,25 +3,38 @@ import 'package:bible/src/model/PassageQuery.dart';
 abstract class Provider {
   final bool _requiresKey;
   final Set<String> _versions;
+  final String name;
 
-  static final Map<String, Provider> _providers = {"esvapi": ESVAPI()};
+  static final List<Provider> _providers = [
+    ESVAPI(),
+  ];
+
+  static final Map<String, Provider> _namedProviders = {};
 
   static final Map<String, Provider> _defaultProviders = {
-    "esv": _providers['esvapi']
+    "esv": _namedProviders['esvapi']
   };
 
-  Provider(this._requiresKey, this._versions);
+  static final Map<String, List<Provider>> _availableProviders = {};
+
+  Provider(this.name, this._requiresKey, this._versions) {
+    _versions.forEach((version) => {
+          Provider._availableProviders.putIfAbsent(version, () => <Provider>[]),
+          Provider._availableProviders[version].add(this)
+        });
+    Provider._namedProviders.putIfAbsent(name, () => this);
+  }
+
   bool containsVersion(String version) => _versions.contains(version);
   bool get requiresKey => _requiresKey;
 
   static getDefaultProvider(String version) => _defaultProviders[version];
-  static getProvider(String provider) => _providers[provider.toLowerCase()];
-
+  static getProvider(String provider) => _namedProviders[provider.toLowerCase];
   PassageQuery getPassage(String query);
 }
 
 class ESVAPI extends Provider {
-  ESVAPI() : super(true, {'esv'});
+  ESVAPI() : super("esvapi", true, {'esv'});
 
   @override
   PassageQuery getPassage(String query) {
