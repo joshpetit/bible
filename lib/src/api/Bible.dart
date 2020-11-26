00206df7892d4299d5499fce3b3a50982f3d7ae1
joshu <joshuapetitma@yahoo.com>
2020-11-26 10:42:44 -0500

    adds keys to Bible

--- lib/src/api/Bible.dart
+++ lib/src/api/Bible.dart
@@ -1,6 +1,15 @@
-import 'package:bible/model/PassageQuery.dart';
+import 'package:bible/src/model/PassageQuery.dart';
+import 'package:reference_parser/reference_parser.dart';
+import 'Provider.dart';
 
 class Bible {
+  static final ESV = Provider.getDefaultProvider('esv');
+  static final Map _keys = <String, String>{};
+
+  static addKeys(Map<String, String> keys) {
+    _keys.addAll(keys);
+  }
+
   static PassageQuery queryPassage(String queryReference) {
     var ref = parseReference(queryReference);
     if (!ref.isValid) {
