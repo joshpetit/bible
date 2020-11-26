e07a7b5850eac7fbdce14006a952dc565f2f7861
joshu <joshuapetitma@yahoo.com>
2020-11-26 07:57:28 -0500

    adds fields and getters for providers + default provers

--- lib/src/api/Provider.dart
+++ lib/src/api/Provider.dart
@@ -4,9 +4,18 @@ abstract class Provider {
   final bool _requiresKey;
   final Set<String> _versions;
 
+  static final Map<String, Provider> _providers = {"esvapi": ESVAPI()};
+
+  static final Map<String, Provider> _defaultProviders = {
+    "esv": _providers['esvapi']
+  };
+
   Provider(this._requiresKey, this._versions);
   bool containsVersion(String version) => _versions.contains(version);
   bool get requiresKey => _requiresKey;
+
+  static getDefaultProvider(String version) => _defaultProviders[version];
+  static getProvider(String provider) => _providers[provider.toLowerCase()];
 
   PassageQuery getPassage(String query);
 }
