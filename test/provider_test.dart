422d1e69479d1f8ae951794f3b208314a94c095b
joshu <joshuapetitma@yahoo.com>
2020-11-27 10:52:07 -0500

    creates basic test format

--- test/provider_test.dart
+++ test/provider_test.dart
@@ -1,16 +1,42 @@
 import 'package:bible/bible.dart';
+import 'package:bible/src/api/Provider.dart';
 import 'package:test/test.dart';
+import 'secrets.dart';
 
 void main() {
-  group('A group of tests', () {
-    Awesome awesome;
-
-    setUp(() {
-      awesome = Awesome();
-    });
-
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
+  group('Test Provider', () {
+    test('Tests that the provider has the correct initialization', () {
+      var s = Provider.getDefaultProvider('esv');
+      expect(s, equals(Bible.ESV));
     });
   });
+
+  group('Test API Providers', () {
+    setUp(() {
+      var keys = getKeys();
+      Bible.addKeys(keys);
+    });
+
+    test('ESV API', () {
+      var passage = Bible.queryPassage('Genesis 1:1',
+          provider: Bible.ESV, parameters: {'include-verse-numbers': 'true'});
+      passage.then((x) => {
+            expect(
+                x.passage,
+                equals(
+                    '[1] In the beginning, God created the heavens and the earth.')),
+          });
+      passage = Bible.queryPassage('Genesis 1:1', provider: Bible.ESV);
+      passage.then((x) => {
+            expect(
+                x.passage,
+                equals(
+                    'In the beginning, God created the heavens and the earth.')),
+          });
+    });
+  });
+}
+
+Map<String, String> getKeys() {
+  return Secrets.keys;
 }
