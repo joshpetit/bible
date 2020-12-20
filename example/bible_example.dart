7425ef7bf7ef6f17af8a53095cb12355c34cb65a
joshu <joshuapetitma@yahoo.com>
2020-12-20 18:25:17 -0500

    updates example :)

--- example/bible_example.dart
+++ example/bible_example.dart
@@ -3,4 +3,7 @@ import 'package:bible/bible.dart';
 void main() {
   Bible.addKeys({'esvapi': 'APITOKEN'});
   var passage = Bible.queryPassage('John 3:16');
+  passage.then((x) => {
+        print(x.passage),
+      });
 }
